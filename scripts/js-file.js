a9eab9a472989452fc440cc7faadbbc8443c5abb
amorfide <73080161+Amorfide@users.noreply.github.com>
2026-01-19 18:09:32 +0000

    added logic to change pokemon sprite at random and reset health bar for each new pokemon that spawns on screen

--- scripts/js-file.js
+++ scripts/js-file.js
@@ -1,9 +1,12 @@
 const screen = document.querySelector(".game-screen-container");
 const healthText = document.getElementById("health-text");
 const healthFill = document.getElementById("health-fill");
+const pokemon = document.querySelector("#pokemon-asset img");
 
 let maxHP = 100;
 let currentHP = 100;
+
+
 
 screen.addEventListener("click", () => {
     currentHP = Math.max(0, currentHP - 10);
@@ -12,4 +15,15 @@ screen.addEventListener("click", () => {
     healthFill.style.width = percent + "%";
 
     healthText.textContent = `${currentHP} / ${maxHP}`;
+
+    if(currentHP === 0){
+        const randomPokemon = Math.floor((Math.random() * 151)+1);
+        pokemon.src=`./assets/pokemon/gen1/${randomPokemon}.png`;
+
+        currentHP = maxHP;
+        healthText.textContent = `${currentHP} / ${maxHP}`;
+
+        const newPercent = (currentHP / maxHP) * 100;
+        healthFill.style.width = newPercent + "%";
+    }
 });
