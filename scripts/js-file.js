b69bb97a6cd973f37402bea7d9dd7b1f52507f2a
amorfide <73080161+Amorfide@users.noreply.github.com>
2026-01-19 17:49:18 +0000

    updated js, previously used a progress bar, now updating the width of the health-fill based on the health left

--- scripts/js-file.js
+++ scripts/js-file.js
@@ -1,6 +1,15 @@
-let health = document.getElementById("health")
 const screen = document.querySelector(".game-screen-container");
+const healthText = document.getElementById("health-text");
+const healthFill = document.getElementById("health-fill");
+
+let maxHP = 100;
+let currentHP = 100;
 
 screen.addEventListener("click", () => {
-    health.value = health.value -10; 
+    currentHP = Math.max(0, currentHP - 10);
+
+    const percent = (currentHP / maxHP) * 100;
+    healthFill.style.width = percent + "%";
+
+    healthText.textContent = `${currentHP} / ${maxHP}`;
 });
